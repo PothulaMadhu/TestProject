f5c14145bb042b02a5dfacd956cd39cb51936b7f
Mreddy <mreddy@blackcurrantapps.com>
2018-05-30 18:43:38 +0530

    Alamofire adding

--- SideMenuSwift/Controllers/HomeVC/HomeVC.swift
+++ SideMenuSwift/Controllers/HomeVC/HomeVC.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeVC: UIViewController, UITabBarDelegate {
 
     @IBOutlet weak var tabBar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkFunction()
         tabBar.delegate = self;
         tabBar.isTranslucent = false
         title = "Home"
@@ -44,4 +46,108 @@ class HomeVC: UIViewController, UITabBarDelegate {
             
         }
     }
+    public func networkFunction() {
+        
+        let headers = [
+            "Authorization": "Basic",
+            "Accept": "application/json"
+        ]
+        let urlRequest = URLRequest(url: URL(string: "https://httpbin.org/get")!)
+        let urlString = urlRequest.url?.absoluteString
+         let parameters: Parameters = ["foo": "bar"]
+        
+        Alamofire.request("https://jsonplaceholder.typicode.com/todos/1", method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseData(queue: DispatchQueue.global(qos: .utility), completionHandler: { (response) in
+            print(response)
+        }).responseJSON(completionHandler: {response in
+            print(response)
+        })
+        // Alamofire 4
+        Alamofire.request("https://jsonplaceholder.typicode.com/todos/1", method: .get, parameters: parameters, encoding: JSONEncoding.default )
+            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                print("Progress: \(progress.fractionCompleted)")
+            }
+            .validate { request, response, data in
+                return .success
+            }
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        
+        //Simple url request
+        Alamofire.request("https://jsonplaceholder.typicode.com/todos/1").response { response in // method defaults to `.get`
+            debugPrint(response)
+        }
+        
+        //Download Request - Simple with URL string // Alamofire 4
+        let destination = DownloadRequest.suggestedDownloadDestination()
+        
+        Alamofire.download("", to: destination).response { response in // method defaults to `.get`
+            print(response.request)
+            print(response.response)
+            print(response.temporaryURL)
+            print(response.destinationURL)
+            print(response.error)
+        }
+        
+        // Alamofire 4
+        Alamofire.download(urlRequest, to: destination).validate().responseData { response in
+            debugPrint(response)
+            print(response.temporaryURL)
+            print(response.destinationURL)
+        }
+        
+        //Download Request - Complex with URL string
+        
+        // Alamofire 4
+        let fileURL: URL
+        let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+            return (fileURL, [.createIntermediateDirectories, .removePreviousFile])
+        }
+        
+        Alamofire.download("", method: .get, parameters: parameters, encoding: JSONEncoding.default, to: destination)
+            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                print("Progress: \(progress.fractionCompleted)")
+            }
+            .validate { request, response, temporaryURL, destinationURL in
+                // Custom evaluation closure now includes file URLs (allows you to parse out error messages if necessary)
+                return .success
+            }
+            .responseJSON { response in
+                debugPrint(response)
+                print(response.temporaryURL)
+                print(response.destinationURL)
+        }
+        
+        //Upload Request - Simple with URL string
+        // Alamofire 4
+        Alamofire.upload(data, to: "").response { response in // method defaults to `.post`
+            debugPrint(response)
+        }
+        
+        // Alamofire 4
+        Alamofire.upload(fileURL, with: urlRequest).validate().responseData { response in
+            debugPrint(response)
+        }
+        
+        //Upload Request - Complex with URL string
+        
+        // Alamofire 4
+        Alamofire.upload(fileURL, to: urlString!, method: .put)
+            .uploadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+                print("Upload Progress: \(progress.fractionCompleted)")
+            }
+            .downloadProgress { progress in // called on main queue by default
+                print("Download Progress: \(progress.fractionCompleted)")
+            }
+            .validate { request, response, data in
+                // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
+                return .success
+            }
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        //https://github.com/Alamofire/Alamofire/blob/master/Documentation/Alamofire%204.0%20Migration%20Guide.md
+    }
 }
+
+
